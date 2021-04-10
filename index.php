3583e80fb2648a47c4a34fdff89c375f17df445e
celikfuurkan <80480296+celikfuurkan@users.noreply.github.com>
2021-04-10 10:57:18 +0300

    index change

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 <p>Welcome Web Programming - Furkan Celik</p>
 ?>
+
+<html lang='en'>
+<head>
+	<title>EXAM</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+		<form method='GET' action='Automobile.php'>
+		
+		<label>LicencePlate : <input type='text' name='licence'></input></label>
+		<label>Brand : <input type='text' name='brand'></input></label>
+		<label>Model : <input type='text' name='model'></input></label>
+		<label>Model Year : <input type='text' name='modelyear'></input></label>
+		<label>Color : <input type='text' name='color'></input></label>
+			
+		<input type='submit' value='SUBMIT'>
+		</form>
+
+</body>
+</html>
